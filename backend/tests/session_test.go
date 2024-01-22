fe21d9557b15ff5df5417e98b93cb25d6d23703a
Asiandayboy <russellcheeseday@gmail.com>
2024-01-22 17:21:36 -0500

    Add test for IsLoggedIn

--- backend/tests/session_test.go
+++ backend/tests/session_test.go
@@ -2,6 +2,8 @@ package tests
 
 import (
 	"backend/api"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -18,13 +20,55 @@ func TestGetSession(t *testing.T) {
 		t.Fatal("Failed to create new session:", err)
 	}
 
-	actualSession, err := sessionManager.GetSession(expectedSession.SessionId.String())
-	if err != nil {
-		t.Fatal("Failed to create new session:", err)
+	actualSession, exists := sessionManager.GetSession(expectedSession.SessionId.String())
+	if !exists {
+		t.Fatal("Session does not exist")
 	}
 
 	// compare memory address of the session
 	if expectedSession != actualSession {
 		t.Fatalf("Expected memAddr: %p, got: %p\n", expectedSession, actualSession)
+	}
+}
+
+func TestIsLoggedIn(t *testing.T) {
+	sessionManager := api.GetSessionManager()
+	session, err := sessionManager.CreateSession() // simulating a loggedIn user
+	if err != nil {
+		t.Fatal("Fail to generate new sessionID when testing")
+	}
+
+	tests := []struct {
+		name           string
+		payload        string
+		expectedReturn bool
+	}{
+		{
+			name:           "Test 1",
+			payload:        "1234597123",
+			expectedReturn: false,
+		},
+		{
+			name:           "Test 2",
+			payload:        session.SessionId.String(),
+			expectedReturn: true,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			r := httptest.NewRequest("POST", "/foo", nil)
+			// add cookie to request
+			r.AddCookie(&http.Cookie{
+				Name:  api.SESSIONID_COOKIE_NAME,
+				Value: tc.payload,
+			})
+
+			_, exists := sessionManager.IsLoggedIn(r)
+			if exists != tc.expectedReturn {
+				t.Fatalf("Expected return: %v, got: %v\n", tc.expectedReturn, exists)
+			}
+
+		})
 	}
 }
