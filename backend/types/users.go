847a21ac8c0514fec9bbf836cae68ec3286b4423
Asiandayboy <russellcheeseday@gmail.com>
2024-01-26 18:18:56 -0500

    Update User fields

--- backend/types/users.go
+++ backend/types/users.go
@@ -1,8 +1,14 @@
 package types
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+/*
+Type used to save into users collections
+*/
 type User struct {
-	Username  string `json:"username"`
-	Password  string `json:"password"`
-	Email     string `json:"email"`
-	SessionId string
+	Username  string             `json:"username"`
+	Password  string             `json:"password"`
+	Email     string             `json:"email"`
+	SessionId string             `json:"sessionid"`
+	UserId    primitive.ObjectID `bson:"_id"`
 }
