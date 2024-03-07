b0bee0561757f3ab7b533bee1161c761535e2484
Asiandayboy <russellcheeseday@gmail.com>
2024-03-07 17:26:19 -0500

    Create ListFurniture component

---
+++ frontend/src/pages/ListFurniture.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+export default function ListFurniture() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        List furniture
+      </main>
+    </>
+  )
+}
