d1e9cd79da78fb3e9830232b35e287e1a1b79873
Asiandayboy <russellcheeseday@gmail.com>
2024-03-13 14:48:14 -0400

    Edit CheckoutSuccess page to include link to Home

--- frontend/src/pages/checkout/CheckoutSuccess.tsx
+++ frontend/src/pages/checkout/CheckoutSuccess.tsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function CheckoutSuccess() {
   return (
-    <div>CheckoutSuccess</div>
+    <div>
+      Your order has successfully been purchased. 
+      You can now close this window.
+      <Link to="/"><button>Click here to return Home</button></Link>
+    </div>
   )
 }
